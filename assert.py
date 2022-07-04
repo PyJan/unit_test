da7306fbdb937ce264be854b108d95423a81c7ae
Jan <jan.svarcbach@gmail.com>
2022-07-04 19:46:01 +0200

    assert as function

--- assert.py
+++ assert.py
@@ -1,3 +1,11 @@
-assert sum([1, 2, 3]) == 6
+def test_sum():
+    assert sum([1, 2, 3]) == 6
 
-print('Run finished.')
+def test_sum2():
+    assert sum((1, 2, 3)) == 5
+
+
+if __name__ == '__main__':
+    test_sum()
+    test_sum2
+    print('job finished')
